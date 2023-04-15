d2eace793cdbef12a78f75861ef46792d2ae0c9f
yosuke-homma <y.homma0708@gmail.com>
2023-04-15 17:00:59 +0900

    RuboCopの指摘をもとに修正

--- spec/request/exam_spec.rb
+++ spec/request/exam_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe 'ExamsController', type: :request do
   describe '#index' do
     it '画面の表示に成功すること' do
       get exams_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe '#new' do
     it '画面の表示に成功すること', openai: true do
       get new_exam_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe '#show' do
     it '画面の表示に成功すること', openai: true do
-      exam = FactoryBot.create(:exam)
+      exam = create(:exam)
       get exam_path exam
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
 
     it 'テストの内容と一致すること', openai: true do
-      exam = FactoryBot.create(:exam)
+      exam = create(:exam)
       get exam_path exam
       expect(response.body).to include exam.question
       expect(response.body).to include exam.answer
