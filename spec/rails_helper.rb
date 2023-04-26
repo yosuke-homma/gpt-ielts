7e707013e0b182550301ab483bef632a892b12c6
yosuke-homma <y.homma0708@gmail.com>
2023-04-26 22:48:56 +0900

    ユーザー認証・認可のテストを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # request specでDeviseを利用するための追加
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
---
+++ spec/request/regisration_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'RegisrationsController', type: :request do
+  describe '#new' do
+    it '画面の表示に成功すること' do
+      get new_user_session_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe '#create' do
+    it '新規ユーザー登録に成功すること' do
+      user_params = FactoryBot.attributes_for(:user)
+      expect {
+        post user_registration_path, params: { user: user_params }
+      }.to change(User, :count).by(1)
+    end
+  end
+
+  describe '#edit' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it '画面の表示に成功すること' do
+      sign_in user
+      get edit_user_registration_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'ログインしていない場合、ログインページにリダイレクトされること' do
+      get edit_user_registration_path
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe '#update' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:update_params) { { name: 'Updated User', current_password: user.password } }
+
+    it 'ユーザー編集に成功すること' do
+      sign_in user
+      patch user_registration_path, params: { user: update_params }
+      expect(user.reload.name).to eq('Updated User')
+    end
+
+    it 'ログインしていない場合、ログインページにリダイレクトされること' do
+      patch user_registration_path
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+end
