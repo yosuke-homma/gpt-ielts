16aaf8f47d6a70042abdf9fbf244247943d850fb
yosuke-homma <y.homma0708@gmail.com>
2023-05-24 14:46:11 +0900

    rubocopの指摘箇所を修正

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Relationships', type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:user) }
-  
+
   before do
     sign_in user
   end
