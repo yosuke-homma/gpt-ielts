f4660a599ee86e356e5a076fc2ae0820afcc7a78
yosuke-homma <y.homma0708@gmail.com>
2023-05-27 20:08:14 +0900

    indexページで全ユーザーの履歴が見れるように修正

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,7 +3,7 @@ class ExamsController < ApplicationController
   before_action :correct_user, except: [:index, :show, :new, :create]
 
   def index
-    @exams = current_user.exams.recent.all.eager_load([:user])
+    @exams = Exam.recent.all.eager_load([:user])
   end
 
   def show
