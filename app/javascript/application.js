1cb45235dc758f705210c56dde47ac4e7bb1f400
yosuke-homma <y.homma0708@gmail.com>
2023-05-28 18:47:35 +0900

    Turbo Driveをデフォルトで有効化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import "@hotwired/turbo-rails"
 import "controllers"
 import "./custom/count"
 import "./custom/countdown"
