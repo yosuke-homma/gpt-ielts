baffded6388d84a2f46089c3958963222befa208
yosuke-homma <y.homma0708@gmail.com>
2024-01-05 22:47:30 +0900

    ユーザー登録・更新の実装とテストを記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,12 +4,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
   respond_to :json
 
+  def update
+    if current_user.update(user_params)
+      render json: {
+        status: { code: 200, message: 'User updated successfully.' },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: { message: "User couldn't be updated successfully. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
       render json: {
-        status: { code: 200, message: 'Signed up successfully.' },
+        status: { code: 201, message: 'Signed up successfully.' },
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }
     else
@@ -17,5 +30,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
         status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/requests/regisrations_spec.rb
+++ spec/requests/regisrations_spec.rb
@@ -1,50 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe 'RegisrationsController', type: :request do
-  describe '#new' do
-    it '画面の表示に成功すること' do
-      get new_user_session_path
-      expect(response).to have_http_status(:ok)
-    end
-  end
-
   describe '#create' do
-    it '新規ユーザー登録に成功すること' do
-      user_params = FactoryBot.attributes_for(:user)
-      expect do
-        post user_registration_path, params: { user: user_params }
-      end.to change(User, :count).by(1)
-    end
-  end
-
-  describe '#edit' do
-    let(:user) { FactoryBot.create(:user) }
-
-    it '画面の表示に成功すること' do
-      sign_in user
-      get edit_user_registration_path
-      expect(response).to have_http_status(:ok)
+    context '正常系' do
+      it '新規ユーザー登録に成功すること' do
+        user_params = FactoryBot.attributes_for(:user)
+        expect do
+          post user_registration_path, params: { user: user_params }
+        end.to change(User, :count).by(1)
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'ログインしていない場合、ログインページにリダイレクトされること' do
-      get edit_user_registration_path
-      expect(response).to redirect_to new_user_session_path
+    context '異常系' do
+      context 'emailが含まれていない場合' do
+        it '新規ユーザー登録に失敗すること' do
+          user_params = FactoryBot.attributes_for(:user)
+          user_params[:email] = ''
+          expect do
+            post user_registration_path, params: { user: user_params }
+          end.not_to change(User, :count)
+          expect(response).to have_http_status '422'
+        end
+      end
+
+      context 'passwordが含まれていない場合' do
+        it '新規ユーザー登録に失敗すること' do
+          user_params = FactoryBot.attributes_for(:user)
+          user_params[:password] = ''
+          expect do
+            post user_registration_path, params: { user: user_params }
+          end.not_to change(User, :count)
+          expect(response).to have_http_status '422'
+        end
+      end
     end
   end
 
   describe '#update' do
     let(:user) { FactoryBot.create(:user) }
-    let(:update_params) { { name: 'Updated User', current_password: user.password } }
 
-    it 'ユーザー編集に成功すること' do
-      sign_in user
-      patch user_registration_path, params: { user: update_params }
-      expect(user.reload.name).to eq('Updated User')
+    context '正常系' do
+      context '名前を変更する場合' do
+        let(:update_params) { { name: 'Updated User' } }
+
+        it 'ユーザー編集に成功すること' do
+          sign_in user
+          patch user_registration_path, params: { user: update_params }
+          expect(user.reload.name).to eq('Updated User')
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'メールアドレスを変更する場合' do
+        let(:update_params) { { email: 'updated_testuser@email.com' } }
+
+        it 'ユーザー編集に成功すること' do
+          sign_in user
+          patch user_registration_path, params: { user: update_params }
+          expect(user.reload.email).to eq('updated_testuser@email.com')
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'パスワードを変更する場合' do
+        let(:update_params) { { password: 'updated_password', password_confirmation: 'updated_password' } }
+
+        it 'ユーザー編集に成功すること' do
+          sign_in user
+          patch user_registration_path, params: { user: update_params }
+          expect(user.reload.password).to eq('updated_password')
+          expect(response).to have_http_status(:success)
+        end
+      end
     end
 
-    it 'ログインしていない場合、ログインページにリダイレクトされること' do
-      patch user_registration_path
-      expect(response).to redirect_to new_user_session_path
+    context '異常系' do
+      let(:update_params) { { name: 'Updated User' } }
+
+      context 'ログインしていない場合' do
+        it 'ユーザー編集に失敗すること' do
+          patch user_registration_path, params: { user: update_params }
+          expect(user.reload.name).to eq('Test User')
+          expect(response).to have_http_status '401'
+          expect(response.body).to eq('You need to sign in or sign up before continuing.')
+        end
+      end
+
+      context '確認用のパスワードが一致しない場合' do
+        let(:update_params) { { password: 'updated_password' } }
+
+        it 'ユーザー編集に成功すること' do
+          sign_in user
+          patch user_registration_path, params: { user: update_params }
+          expect(response).to have_http_status '422'
+        end
+      end
     end
   end
 end
